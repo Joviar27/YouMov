2d432abb97257178e3a44e6af9ce07b1ea36d455
joviar <joy@samir.co.id>
2026-09-19 14:32:01 +0700

    feat: setup font, color, theme

--- app/src/main/java/com/cobasendiri/youmov/ui/theme/Type.kt
+++ app/src/main/java/com/cobasendiri/youmov/ui/theme/Type.kt
@@ -1,34 +1,67 @@
 package com.cobasendiri.youmov.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cobasendiri.youmov.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val PoppinsFontFamily: FontFamily
+    @Composable get() = FontFamily (
+        Font(R.font.poppins_bold, weight = FontWeight.Bold),
+        Font(R.font.poppins_semibold, weight = FontWeight.SemiBold),
+        Font(R.font.poppins_medium, weight = FontWeight.Medium),
+        Font(R.font.poppins_regular, weight = FontWeight.Normal)
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+val baseline = Typography()
+
+@Composable
+fun youMovTypography(): Typography{
+    val poppinsFontFamily = PoppinsFontFamily
+    return Typography(
+        displayLarge = baseline.displayLarge.copy(fontFamily = poppinsFontFamily),
+        displayMedium = baseline.displayMedium.copy(fontFamily = poppinsFontFamily),
+        displaySmall = baseline.displaySmall.copy(fontFamily = poppinsFontFamily),
+        headlineLarge = baseline.headlineLarge.copy(fontFamily = poppinsFontFamily),
+        headlineMedium = baseline.headlineMedium.copy(fontFamily = poppinsFontFamily),
+        headlineSmall = baseline.headlineSmall.copy(
+            fontFamily = poppinsFontFamily,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = TextPrimary
+        ),
+        titleLarge = baseline.titleLarge.copy(fontFamily = poppinsFontFamily),
+        titleMedium = baseline.titleMedium.copy(
+            fontFamily = poppinsFontFamily,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = TextPrimary
+        ),
+        titleSmall = baseline.titleSmall.copy(
+            fontFamily = poppinsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            color = TextPrimary
+        ), //14.sp,
+        bodyLarge = baseline.bodyLarge.copy(fontFamily = poppinsFontFamily),//16.sp
+        bodyMedium = baseline.bodyMedium.copy(
+            fontFamily = poppinsFontFamily,
+            fontWeight = FontWeight.Normal,
+            color = TextSecondary
+        ),//14.sp
+        bodySmall = baseline.bodySmall.copy(fontFamily = poppinsFontFamily),
+        labelLarge = baseline.labelLarge.copy(fontFamily = poppinsFontFamily),
+        labelMedium = baseline.labelMedium.copy(
+            fontFamily = poppinsFontFamily,
+            fontWeight = FontWeight.Medium,
+            color = TextMuted
+        ),//12.sp
+        labelSmall = baseline.labelSmall.copy(
+            fontFamily = poppinsFontFamily,
+            fontWeight = FontWeight.Normal,
+            color = TextMuted
+        ),//11.sp
     )
-    */
-)
+}
